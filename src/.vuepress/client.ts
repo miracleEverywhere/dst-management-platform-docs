f8d7c1acf3f661df7202e9f51866d3117f1339b7
miracleEverywhere <763483966@qq.com>
2026-04-24 11:41:13 +0800

    给edge擦钩子

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -2,6 +2,7 @@ import { defineClientConfig } from "vuepress/client";
 
 export default defineClientConfig({
   setup() {
+    if (__VUEPRESS_SSR__) return;
     window.history.replaceState = () => {};
   },
 });
